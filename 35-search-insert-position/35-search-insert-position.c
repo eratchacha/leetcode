d393f670feb520970638072bb36aa962933bda8e
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2022-09-26 11:35:39 +0900

    Time: 15 ms (7.46%), Space: 5.9 MB (80.43%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.c
@@ -0,0 +1,12 @@
+
+
+int searchInsert(int* nums, int numsSize, int target){
+    int i = 0;
+    while (i < numsSize)
+    {
+        if (nums[i] >= target)
+            return i;
+        i++;
+    }
+    return numsSize;
+}
